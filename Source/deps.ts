f178d3b2d99d6baba58736e3155c758136168efd
ElectronicsArchiver <85485984+ElectronicsArchiver@users.noreply.github.com>
2022-06-28 14:32:03 -0400

    Updated Dependencies

--- Source/deps.ts
+++ Source/deps.ts
@@ -1,12 +1,12 @@
 
 export { Sync } from 'https://raw.githubusercontent.com/txthinking/denolib/master/f.js'
 
-export { encode as hexencode , decode as hexdecode } from 'https://deno.land/std@0.130.0/encoding/hex.ts';
-export { yellow , bgGreen } from 'https://deno.land/std@0.130.0/fmt/colors.ts';
-export { randomBytes } from 'https://deno.land/std@0.130.0/node/crypto.ts';
-export { createHash } from 'https://deno.land/std@0.130.0/hash/mod.ts';
-export { serve } from 'https://deno.land/std@0.130.0/http/server.ts'
-export { join } from 'https://deno.land/std@0.130.0/path/mod.ts'
+export { encode as hexencode , decode as hexdecode } from 'https://deno.land/std@0.145.0/encoding/hex.ts';
+export { yellow , bgGreen } from 'https://deno.land/std@0.145.0/fmt/colors.ts';
+export { randomBytes } from 'https://deno.land/std@0.145.0/node/crypto.ts';
+export { createHash } from 'https://deno.land/std@0.145.0/hash/mod.ts';
+export { serve } from 'https://deno.land/std@0.145.0/http/server.ts'
+export { join } from 'https://deno.land/std@0.145.0/path/mod.ts'
 
 export { connect } from 'https://deno.land/x/redis@v0.26.0/mod.ts'
 export { Client } from 'https://deno.land/x/mysql@v2.10.2/mod.ts'
